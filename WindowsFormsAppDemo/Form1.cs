7d0eebf3054d025c1feebf6253aacb8be5de9a59
GabyGiles <info@gabrielagiles.com.ar>
2022-10-18 03:15:57 -0300

    cambio

--- WindowsFormsAppDemo/Form1.cs
+++ WindowsFormsAppDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsAppDemo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("dddd");
+        }
     }
 }
